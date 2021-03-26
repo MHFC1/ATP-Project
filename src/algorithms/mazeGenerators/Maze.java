14f819af86090df9961dbdf2cac4d6cfe51d12fb
LIDORZ55 <lidorzurzur@gmail.com>
2021-03-26 13:55:21 +0300

    3 new classes

---
+++ src/algorithms/mazeGenerators/Maze.java
@@ -0,0 +1,19 @@
+package algorithms.mazeGenerators;
+
+public class Maze implements IMaze {
+    private int [][] mazeStruct;
+
+    public Position getStartPosition() {
+        return null;
+    }
+
+    @Override
+    public Position getGoalPosition() {
+        return null;
+    }
+
+    @Override
+    public void Print() {
+
+    }
+}
